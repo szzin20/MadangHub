550d14388bf513b618620cf0fd7303d4127df3af
szzin20 <92289030+szzin20@users.noreply.github.com>
2023-10-27 20:23:11 +0700

    fixing middlewares

--- middlewares/log.go
+++ middlewares/log.go
@@ -1,27 +1,12 @@
-package middleware
+package middlewares
 
 import (
-	"log"
-	"time"
-
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-func Logger() echo.MiddlewareFunc {
-	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			start := time.Now()
-
-			err := next(c)
-
-			end := time.Now()
-			latency := end.Sub(start)
-			latencyHuman := latency.String()
-			status := c.Response().Status
-
-			log.Printf("[%s] %s %s %s %d %s", end.Format("2006/01/02 - 15:04:05"), c.RealIP(), c.Request().Method, c.Path(), status, latencyHuman)
-
-			return err
-		}
-	}
+func LogMiddleware(e *echo.Echo) {
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}, latency_human=${latency_human}\n",
+	}))
 }
