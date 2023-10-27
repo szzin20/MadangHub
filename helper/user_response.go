98319c61b6f324f073a9fe252c3d0d19932ec483
szzin20 <92289030+szzin20@users.noreply.github.com>
2023-10-27 23:57:53 +0700

    fixing

--- helper/user_response.go
+++
@@ -1,9 +0,0 @@
-package helper
-
-type UserResponse struct {
-	UserID uint   `json:"id"`
-	Name   string `json:"name"`
-	Email  string `json:"email"`
-	Phone  string `json:"phone"`
-	Token  string `json:"token"`
-}
