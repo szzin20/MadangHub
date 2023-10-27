98fdaaa8d4a6d98b61a1bf0be80d2a2884d1a98f
szzin20 <92289030+szzin20@users.noreply.github.com>
2023-10-28 00:00:51 +0700

    added config

---
+++ config/route.go
@@ -0,0 +1,19 @@
+package config
+
+// import (
+// 	"log"
+
+// 	"github.com/labstack/echo/v4"
+// 	"github.com/labstack/echo/v4/middleware"
+// )
+
+// func SetupRouter() *echo.Echo {
+// 	db, err := InitializeDatabase()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	router := echo.New()
+// 	router.Use(middleware.Logger())
+// 	routes.SetupRoutes(router, db)
+// 	return router
+// }
