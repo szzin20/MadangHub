5d2c5fbc9cf82dbdb07160bfd98a872379f6299f
szzin20 <92289030+szzin20@users.noreply.github.com>
2023-10-31 13:00:43 +0700

    added ci cd

--- app/config/config.go
+++ app/config/config.go
@@ -5,7 +5,6 @@ import (
 	"strconv"
 
 	"github.com/joho/godotenv"
-	"github.com/sirupsen/logrus"
 	"gorm.io/gorm"
 )
 
@@ -26,10 +25,10 @@ func InitConfig() *AppConfig {
 	var res = new(AppConfig)
 	res = loadConfig()
 
-	if res == nil {
-		logrus.Fatal("Config : Cannot start program, failed to load configuration")
-		return nil
-	}
+	// if res == nil {
+	// 	logrus.Fatal("Config : Cannot start program, failed to load configuration")
+	// // 	return nil
+	// // }
 
 	return res
 }
@@ -37,12 +36,12 @@ func InitConfig() *AppConfig {
 func loadConfig() *AppConfig {
 	var res = new(AppConfig)
 
-	err := godotenv.Load(".env")
+	godotenv.Load(".env")
 
-	if err != nil {
-		logrus.Error("Config : Cannot load config file,", err.Error())
-		return nil
-	}
+	// if err != nil {
+	// 	logrus.Error("Config : Cannot load config file,", err.Error())
+	// 	return nil
+	// }
 
 	if val, found := os.LookupEnv("SERVERPORT"); found {
 		port, err := strconv.Atoi(val)
